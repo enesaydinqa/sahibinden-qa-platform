d0806053b3b8299206add2eb8c299a1cb972f14c
Enes Aydin <enes.aydin@sahibinden.com>
2020-11-13 22:31:59 +0300

    device info

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {page:'Testbox', menuId:'testbox'});
+router.get('/', function (req, res, next) {
+    res.render('index', {page: 'Testbox', menuId: 'testbox'});
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About Us', menuId:'about'});
+router.get('/device', function (req, res, next) {
+    res.render('device', {page: 'Device Info', menuId: 'device'});
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', {page:'Contact Us', menuId:'contact'});
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {page: 'Contact Us', menuId: 'contact'});
 });
 
 module.exports = router;
