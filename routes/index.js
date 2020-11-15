2d96364abe1704f62ea3a8cfa6c932bfc9e4048c
Enes Aydin <enes.aydin@sahibinden.com>
2020-11-15 14:17:41 +0300

    db config added

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 var express = require('express');
+var mysqlpool = require('../dbconfig');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {page: 'Testbox', menuId: 'testbox'});
+    mysqlpool.getConnection(function (err, connection) {
+        connection.query('SELECT * FROM testbox', function (error, results, fields) {
+            connection.release();
+            if (error) throw error; 
+            res.render('index', { page: 'Testbox', menuId: 'testbox', data: JSON.stringify(results, null, 4)});
+        });
+    });
 });
 
 router.get('/device', function (req, res, next) {
-    res.render('device', {page: 'Device Info', menuId: 'device'});
+    res.render('device', { page: 'Device Info', menuId: 'device' });
 });
 
 router.get('/contact', function (req, res, next) {
-    res.render('contact', {page: 'Contact Us', menuId: 'contact'});
+    res.render('contact', { page: 'Contact Us', menuId: 'contact' });
+});
+
+router.post('/testbox/update/:userId', function (req, res) {
+    console.log(req.params.userId)
+    res.statusCode(200)
 });
 
 module.exports = router;
