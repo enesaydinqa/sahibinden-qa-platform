9449ea6e939af286a470c2399654498d01023794
Enes Aydin <enes.aydin@admin-MacBook-Pro.local>
2020-12-06 14:06:55 +0300

    extension

---
+++ public/javascripts/extension.js
@@ -0,0 +1,22 @@
+function saveExtension() {
+
+    var extensionName = document.getElementById("name").value;
+    var extensionDesc = document.getElementById("description").value;
+    var extensionLink = document.getElementById("link").value;
+
+    $.ajax({
+        url: "/extension/save",
+        type: "POST",
+        data: JSON.stringify({ name: extensionName, description: extensionDesc, link: extensionLink }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data, textStatus, jqXHR) {
+            location.reload()
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+
+        }
+    });
+    ß
+
+}
