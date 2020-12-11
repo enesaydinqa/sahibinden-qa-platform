5e3cc515c8b9b2b2788c75413a4a549157eae8c6
Enes Aydin <enes.aydin@admin-MacBook-Pro.local>
2020-12-11 12:35:23 +0300

    major update

--- public/javascripts/ios.js
+++ public/javascripts/ios.js
@@ -1,21 +1,23 @@
-var ws
-
 function start(el) {
+    for (loop = 0; loop < 100; loop++) {
+        var devices = [
+            "c41e0c25fe4c0ce0ef28cf8d4862e08ce5ac46c9",
+            "0d123e38d24c6df3d198b576fd6f7396ed46978a",
+            "12b0530d88260b3211a4f360ea42c8c945ddeae4",
+            "00008030-0018306102E0402E",
+            "00008030-0002199E0ED3802E",
+            "00008030-000451410E87802E"
+        ];
 
-    if (el.checked) {
-        ws = new WebSocket('ws://' + location.host + '/');
-
-        ws.onopen = function() {
-            console.log('WebSocket Client Connected');
-            ws.send(el.checked);
-        };
-
-        ws.onmessage = function(e) {
-            var data = JSON.parse(e.data);
-            console.log(data)
-            document.getElementById(data[0]).src = "data:image/jpg;base64," + data[1]
-        };
-    } else {
-        ws.send(false)
+        for (i = 0; i < devices.length; i++) {
+            $.ajax({
+                url: "/ios/getDeviceBase64",
+                type: "POST",
+                data: JSON.stringify({ uid: devices[i] }),
+                contentType: "application/json; charset=utf-8",
+                success: function(data, textStatus, jqXHR) {},
+                error: function(jqXHR, textStatus, errorThrown) {}
+            });
+        }
     }
 }
--- routes/ios.js
+++ routes/ios.js
@@ -4,4 +4,9 @@ router.get('/', function(req, res, next) {
     res.render('ios', { page: 'IOS', menuId: 'IOS' });
 });
 
+router.post('/getDeviceBase64', async function(req, res, next) {
+    console.log(req.body.uid)
+    await res.send("test")
+});
+
 module.exports = router;
