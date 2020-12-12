412da208e30b9af260ca62a9fc4d9010bb03d880
Enes Aydin <enes.aydin@admin-MacBook-Pro.local>
2020-12-12 18:58:32 +0300

    update

--- public/javascripts/ios.js
+++ public/javascripts/ios.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-function start(el) {
-    for (loop = 0; loop < 100; loop++) {
-        var devices = [
-            "c41e0c25fe4c0ce0ef28cf8d4862e08ce5ac46c9",
-            "0d123e38d24c6df3d198b576fd6f7396ed46978a",
-            "12b0530d88260b3211a4f360ea42c8c945ddeae4",
-            "00008030-0018306102E0402E",
-            "00008030-0002199E0ED3802E",
-            "00008030-000451410E87802E"
-        ];
-
-        for (i = 0; i < devices.length; i++) {
-            $.ajax({
-                url: "/ios/getDeviceBase64",
-                type: "POST",
-                data: JSON.stringify({ uid: devices[i] }),
-                contentType: "application/json; charset=utf-8",
-                success: function(data, textStatus, jqXHR) {},
-                error: function(jqXHR, textStatus, errorThrown) {}
-            });
-        }
-=======
 var ws = null;
 
 async function wait(ms) {
@@ -69,6 +46,5 @@ async function start(el) {
         ws.send(false)
         ws.close();
         location.reload();
->>>>>>> Stashed changes
     }
 }
--- routes/ios.js
+++ routes/ios.js
@@ -5,11 +5,6 @@ router.get('/', function(req, res, next) {
     res.render('ios', { page: 'IOS', menuId: 'IOS' });
 });
 
-<<<<<<< Updated upstream
-router.post('/getDeviceBase64', async function(req, res, next) {
-    console.log(req.body.uid)
-    await res.send("test")
-=======
 router.post('/getDeviceImage', function(req, res, next) {
     let sql = 'SELECT base64 FROM phones WHERE uid = ?';
     let data = [req.body.uid];
@@ -18,7 +13,6 @@ router.post('/getDeviceImage', function(req, res, next) {
         response = { base64: rows[0].base64, status: 200 };
         res.end(JSON.stringify(response));
     });
->>>>>>> Stashed changes
 });
 
 module.exports = router;
