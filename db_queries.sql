5dd3453c614cb058916c04237427ea01cac8b5ed
Enes Aydin <enes.aydin@enesaydin.local>
2021-01-27 09:45:06 +0300

    update

--- db_queries.sql
+++ db_queries.sql
@@ -4,46 +4,59 @@ use sahibinden_qa CREATE TABLE testbox (id smallint unsigned not null auto_incre
 
 ALTER TABLE testbox CONVERT TO CHARACTER SET utf8;
 
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (1, 'ENES AYDIN', 'no issue', null, 'Mobiletest05', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (2, 'TOLGA MOLLA', 'no issue', null, 'Mobiletest06', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (3, 'TOLGA MOLLA', 'no issue', null, 'Mobiletest03', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (5, 'TOLGA MOLLA', '', null, 'Mobiletest09', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (6, 'Enes Aydın', '', null, 'Mobiletest10', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (17, 'Esra Köse', '', null, 'Mobiletest13', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (19, 'Esra Köse', '', null, 'Mobiletest14', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (37, 'Esra Köse', '', null, 'Mobiletest15', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (51, 'Esra Köse', '', null, 'Mobiletest19', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (61, 'Esra Köse', '', null, 'Mobiletest16', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (62, 'Esra Köse', '', null, 'Mobiletest17', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (63, 'Esra Köse', '', null, 'Mobiletest07', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (64, 'Esra Köse', '', null, 'Mobiletest08', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (76, 'Esra Köse', '', null, 'Mobiletest11', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (82, 'Esra Köse', '', null, 'CRM', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (92, 'Esra Köse', '', null, 'Mobiletest18', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (99, 'Esra Köse', '', null, 'Mobiletest20', 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (130, 'Esra Köse', '', null, null, 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (127, 'Esra Köse', '', null, null, 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (132, 'Esra Köse', '', null, null, 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (133, 'Esra Köse', '', null, null, 'GENERAL', 0);
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (134, 'Esra Köse', '', null, null, 'GENERAL', 0);
-
-
-
-
-INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type,in_use) VALUES (65, null, 'no issue', null, null, 'RELEASE', 0);
-
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (1, 'Please Select', '', null, 'Mobiletest05', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (2, 'Please Select', '', null, 'Mobiletest06', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (3, 'Please Select', '', null, 'Mobiletest03', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (5, 'Please Select', '', null, 'Mobiletest09', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (6, 'Please Select', '', null, 'Mobiletest10', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (17, 'Please Select', '', null, 'Mobiletest13', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (19, 'Please Select', '', null, 'Mobiletest14', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (37, 'Please Select', '', null, 'Mobiletest15', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (51, 'Please Select', '', null, 'Mobiletest19', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (61, 'Please Select', '', null, 'Mobiletest16', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (62, 'Please Select', '', null, 'Mobiletest17', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (63, 'Please Select', '', null, 'Mobiletest07', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (64, 'Please Select', '', null, 'Mobiletest08', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (76, 'Please Select', '', null, 'Mobiletest11', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (82, 'Please Select', '', null, 'CRM', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (92, 'Please Select', '', null, 'Mobiletest18', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (99, 'Please Select', '', null, 'Mobiletest20', 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (130, 'Please Select', '', null, null, 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (131, 'Please Select', '', null, null, 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (132, 'Please Select', '', null, null, 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (133, 'Please Select', '', null, null, 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (134, 'Please Select', '', null, null, 'GENERAL', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (65, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (85, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (7, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (8, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (9, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (10, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (11, 'Please Select', '', null, null, 'RELEASE', 0);
+INSERT INTO testbox (testbox_id, testbox_use, issue, description, mobile_test, testbox_type, in_use) VALUES (12, 'Please Select', '', null, null, 'RELEASE', 0);
 
 
 use sahibinden_qa CREATE TABLE qa_user (id smallint unsigned not null auto_increment,  name_surname varchar(100),  constraint pk_example primary key (id));
 
 ALTER TABLE qa_user CONVERT TO CHARACTER SET utf8;
 
-INSERT INTO qa_user (name_surname) VALUES ('Enes Aydin');
-INSERT INTO qa_user (name_surname) VALUES ('Tolga Molla');
 INSERT INTO qa_user (name_surname) VALUES ('Mustafa Ertürk');
-INSERT INTO qa_user (name_surname) VALUES ('Pinar Ormeci');
-INSERT INTO qa_user (name_surname) VALUES ('Berivan Göktas');
-INSERT INTO qa_user (name_surname) VALUES ('Hasan Can Yildir');
+INSERT INTO qa_user (name_surname) VALUES ('Savaş Avcu');
+INSERT INTO qa_user (name_surname) VALUES ('Esra Köse');
+INSERT INTO qa_user (name_surname) VALUES ('Ceren Sarıçam');
+INSERT INTO qa_user (name_surname) VALUES ('Mehtap Ertem');
+INSERT INTO qa_user (name_surname) VALUES ('Sevinç Sarı');
+INSERT INTO qa_user (name_surname) VALUES ('Çağla Çelik');
+INSERT INTO qa_user (name_surname) VALUES ('Yağmur Kılıç');
+INSERT INTO qa_user (name_surname) VALUES ('Tuba Uluğ');
+INSERT INTO qa_user (name_surname) VALUES ('Halil Çifteci');
+INSERT INTO qa_user (name_surname) VALUES ('Burak Ergören');
+INSERT INTO qa_user (name_surname) VALUES ('Tolga Molla');
+INSERT INTO qa_user (name_surname) VALUES ('Enes Aydın');
+INSERT INTO qa_user (name_surname) VALUES ('Pınar Ormeci');
+INSERT INTO qa_user (name_surname) VALUES ('Hasan Can Yıldır');
+INSERT INTO qa_user (name_surname) VALUES ('Nisan Derman');
+INSERT INTO qa_user (name_surname) VALUES ('Sezai Can');
 
 
 use sahibinden_qa CREATE TABLE tags (id smallint unsigned not null auto_increment, data_content varchar(1000000), constraint pk_example primary key (id));
--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,19 @@ async function wait(ms) {
 }
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/', async function(req, res, next) {
 
     var testboxInfo;
     var qaUsers = [];
 
-    db_query_execute('SELECT * FROM testbox ORDER BY testbox_id ASC', function (err, rows) {
+    db_query_execute('SELECT * FROM testbox ORDER BY testbox_id ASC', function(err, rows) {
         if (err) res.render('error');
         testboxInfo = rows;
     });
 
-    db_query_execute('SELECT name_surname FROM qa_user', function (err, rows) {
+    db_query_execute('SELECT name_surname FROM qa_user ORDER BY name_surname ASC', function(err, rows) {
         if (err) res.render('error');
-        Object.keys(rows).forEach(function (key) { qaUsers.push(rows[key]); });
+        Object.keys(rows).forEach(function(key) { qaUsers.push(rows[key]); });
     });
 
     await wait(2000);
@@ -28,7 +28,7 @@ router.get('/', async function (req, res, next) {
     res.render('index', { page: 'Testbox', menuId: 'testbox', name: qaUsers, data: testboxInfo });
 });
 
-router.post('/testbox/update', function (req, res) {
+router.post('/testbox/update', function(req, res) {
     var testboxUse = req.body.testboxUse;
     var inUse = (testboxUse == "Please Select") ? 0 : 1;
 
@@ -36,19 +36,19 @@ router.post('/testbox/update', function (req, res) {
 
     let data = [req.body.testboxUse, req.body.issueNumber, req.body.issueDescription, inUse, req.body.testbox];
 
-    db_query_execute(sql, data, function (err, rows) {
+    db_query_execute(sql, data, function(err, rows) {
         response = { message: 'Successfully', status: 200 };
         res.end(JSON.stringify(response));
     });
 });
 
-router.post('/testbox/release', function (req, res) {
+router.post('/testbox/release', function(req, res) {
     var testboxNumber = req.body.testbox;
 
     let sql = `UPDATE testbox SET testbox_use = 'Please Select', issue = '', description = '', in_use = 0 WHERE testbox_id = ?`;
     let data = [testboxNumber];
 
-    db_query_execute(sql, data, function (err, rows) {
+    db_query_execute(sql, data, function(err, rows) {
         response = { message: testboxNumber + ' : Testbox released', status: 200 };
         res.end(JSON.stringify(response));
     });
